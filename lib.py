e5d986505b9b0d51d8b79d74a833736a3896da11
Jeeru Harshith Reddy <143481508+reddy-j-harshith@users.noreply.github.com>
2025-04-24 14:26:56 +0530

    Delete lib.py

--- lib.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline ,AutoTokenizer, AutoModelForSeq2SeqLM
-import torch,sacrebleu,nltk
-from rouge_score import rouge_scorer
-nltk.download('wordnet')
-from nltk.translate.meteor_score import meteor_score
-from datasets import load_dataset
